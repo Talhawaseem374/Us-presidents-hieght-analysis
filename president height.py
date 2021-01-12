3b6cb8b46136fd449026c77d4f095c180d2737f4
Talha Waseem <77092369+Talhawaseem374@users.noreply.github.com>
2021-01-12 11:32:03 +0500

    Add files via upload

---
+++ president height.py
@@ -0,0 +1,26 @@
+# Importing numpy and pandas
+import numpy as np
+import pandas as pd
+
+
+data = pd.read_csv("C:\\Users\\CR2-13\\Downloads\\president_heights.csv")
+print(data.head())
+
+height = np.array(data["height(cm)"])
+print(height)
+print("Mean of heights =", height.mean())
+print("Standard Deviation of height =", height.std())
+print("Minimum height =", height.min())
+print("Maximum height =", height.max())
+
+print("25th percentile =", np.percentile(height, 25))
+print("Median =", np.median(height))
+print("75th percentile =", np.percentile(height, 75))
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+plt.hist(height)
+plt.title("Height Distribution of Presidents of USA")
+plt.xlabel("height(cm)")
+plt.ylabel("Number")
+plt.show()
